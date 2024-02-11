19a31d52363a5ea0f409baded3da613277157dbb
Raja <rajasaravanan2003@gmail.com>
2024-02-11 21:44:04 +0530

    asked inputs for add and sub and displayed

--- trail1.py
+++ trail1.py
@@ -1,7 +1,15 @@
 #function add
-def add():{}
+def add():
+    print("so you picked addition uh? alright give me the numbers ")
+    b=int(input("..."))
+    c=int(input("..."))
+    print(f"alright,lets add {b} and {c}")
 #function sub
-def sub():{}
+def sub():
+    print("so you picked subtraction uh? alright give me the numbers ")
+    b=int(input("..."))
+    c=int(input("..."))
+    print(f"alright,lets subtract {b} and {c}")
 #func main for add and sub
 def main():
     add()
